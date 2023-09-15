28c64497f98d0f85106ef5b0b66c4723b80b3af8
Edison <edisonymy@gmail.com>
2023-09-15 23:39:39 +0100

    ga

---
+++ src/utils/ga.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import shutil
+import pathlib
+import logging
+import streamlit as st
+
+
+def add_analytics_tag():
+    # replace G-QSN0R08N2M to your web app's ID
+    
+    analytics_js = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QSN0R08N2M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QSN0R08N2M');
+</script>
+    """
+    analytics_id = "G-QSN0R08N2M"
+
+    
+    # Identify html path of streamlit
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=analytics_id): # if id not found within html file
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # backup recovery
+        else:
+            shutil.copy(index_path, bck_index)  # save backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
+        index_path.write_text(new_html)
